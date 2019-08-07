cb8ccbb891681d610db9fca20044cdf5490646d8
WoBo55 <53507794+WoBo55@users.noreply.github.com>
2019-08-07 11:05:25 +0200

    Wichtige Datenfelder für die Registrierung von Benutzenden
    Die Datenfelder für Vorname und Nachname werden für die Registrierung benötigt, da ansonsten kein korrekter Eintrag in die Tabelle erfolgen kann.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,6 +50,8 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            'firstname' => ['required', 'string', 'max:255'],
+            'lastname' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -65,6 +67,8 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'firstname' => $data['firstname'],
+            'lastname' => $data['lastname'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
