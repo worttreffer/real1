dee66aa44b1729eb7da4976f5ffda96f3b8fb7d1
WoBo55 <53507794+WoBo55@users.noreply.github.com>
2019-08-08 12:01:07 +0200

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('firstname');
             $table->string('lastname');
-            $table->bigInteger('addresses_id');
+            $table->bigInteger('addresses_id')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
