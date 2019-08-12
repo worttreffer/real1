f278e02363bf8132ee6040e278faf3d56eb3185b
Wolfgang Boos <wolfgang_boos@gmx.de>
2019-08-12 10:53:22 +0200

    Umstellung auf Deutsch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,10 +70,10 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">Anmelden</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">Registrierung</a>
                         @endif
                     @endauth
                 </div>
