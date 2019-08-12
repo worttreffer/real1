4405a21820b3d73e09587b657816323597bd96cd
Wolfgang Boos <wolfgang_boos@gmx.de>
2019-08-12 08:59:13 +0200

    Worttreffer statt laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Worttreffer
                 </div>
 
                 <div class="links">
