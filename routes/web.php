0bef4b9c67d3efd15f2da99e75dc783687ce5eb3
Bastian Schülke <bas.schuelke@googlemail.com>
2019-08-12 19:03:25 +0200

    Creating controller for profile types

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Security
 Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Overview related
 
+
+// Basic Profil relatad
+Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/home/saveProfile', 'HomeController@saveAddress');
 
+// Event Profil related
+Route::get('/home/event/{id}', 'EventController@index');
+Route::get('/home/event/{id}/edit', 'EventController@edit');
+Route::post('/home/event/{id}/save', 'EventController@save');
+Route::post('/home/event/{id}/delete', 'EventController@delete');
 
+// Artist Profil related
+Route::get('/home/artist/{id}', 'ArtistController@index');
+Route::get('/home/artist/{id}/edit', 'ArtistController@edit');
+Route::post('/home/event/{id}/save', 'ArtistController@save');
+Route::post('/home/artist/{id}/delete', 'ArtistController@delete');
+
+// Location Profil related
+Route::get('/home/location/{id}', 'LocationController@index');
+Route::get('/home/location/{id}/edit', 'LocationController@edit');
+Route::post('/home/location/{id}/save', 'LocationController@save');
+Route::post('/home/location/{id}/delete', 'LocationController@delete');
+
+// Static Pages
 Route::get('/impressum', function () {
     return view('static/imprint');
-});
+})->name('imprint');
