a13d72b7cea4f6e2d02e5018b81017c0aba7a25e
Wolfgang Boos <wolfgang_boos@gmx.de>
2019-08-13 10:49:07 +0200

    Nachrichten an Benutzer

---
+++ app/Notifications/fallower.php
@@ -0,0 +1,61 @@
+﻿<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class fallower extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Die Daten für das Standard-Newsletter wurden angelegt.')
+                    ->action('Änderungen bitte, falls erforderlich, vornehmen.', url('/'))
+                    ->line('Danke für das uns entgegen gebrachte Vertrauen.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
