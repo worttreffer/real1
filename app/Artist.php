39ede07ab36400fb2d94ce75fb18f8585a4a20f4
Wolfgang Boos <wolfgang_boos@gmx.de>
2019-08-28 08:59:50 +0200

    Anpassung an DB

--- app/Artist.php
+++ app/Artist.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Artist extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'description', 'image', 'bookable'
+    ];    //
 }
--- app/Filter.php
+++ app/Filter.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Filter extends Model
 {
-    //
+    protected $fillable = [
+        'km', 'km_post_code', 'city', 'postcode', 'state', 'region', 'country', 'valid_from', 'valid_to'
+    ];//
 }
